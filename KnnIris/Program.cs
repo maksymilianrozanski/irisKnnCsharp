bc9a04704f48753e1692ae78584fe360c8651ec2
Max <maksymilianrozanski@gmail.com>
2020-05-31 18:09:37 +0200

    add manual and reading url from console

--- KnnIris/Program.cs
+++ KnnIris/Program.cs
@@ -9,17 +9,31 @@ namespace KnnIris
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter url to training data csv, or press Enter twice to use Iris dataset");
+            Console.WriteLine("csv format using ',' as value separator and '.' as decimal point separator is expected");
+            Console.WriteLine("csv must not contain table header");
+            Console.WriteLine("data label in the last column is expected");
 
-            const string trainingDataUrl =
-                "https://gist.githubusercontent.com/maksymilianrozanski/fa386a964426d8b0b8fac75a026a9dbd/raw/b5aa5f57df07a4e8ecec6ed9a7eeba5ac8fe9aa3/irisTrainingData";
-            const string validationDataUrl =
+            const string irisTrainingDataUrl =
+            "https://gist.githubusercontent.com/maksymilianrozanski/fa386a964426d8b0b8fac75a026a9dbd/raw/b5aa5f57df07a4e8ecec6ed9a7eeba5ac8fe9aa3/irisTrainingData";
+            const string irisValidationDataUrl =
                 "https://gist.githubusercontent.com/maksymilianrozanski/3b631b25cccc2baa2731956aaecc7716/raw/1d62a26bdeaf91fe4dcaf56d1fa96de13ab75e5a/irisValidationData";
 
-            var downloader = new CsvDownloader();
+            var trainingDataUrl = Console.ReadLine();
+            var validationDataUrl = Console.ReadLine();
 
-            var trainingDataCsv = downloader.DownloadFromUrl(new Uri(trainingDataUrl));
-            var validationDataCsv = downloader.DownloadFromUrl(new Uri(validationDataUrl));
+            (Either<NetworkRequestError, string>, Either<NetworkRequestError, string>) DownloadedData(string url1,
+                string url2)
+            {
+                var downloader = new CsvDownloader();
+                return url1 == "" || url2 == ""
+                    ? (downloader.DownloadFromUrl(new Uri(irisTrainingDataUrl)),
+                        downloader.DownloadFromUrl(new Uri(irisValidationDataUrl)))
+                    : (downloader.DownloadFromUrl(new Uri(url1)), downloader.DownloadFromUrl(new Uri(url2)));
+            }
+
+            var (trainingDataCsv, validationDataCsv) =
+                DownloadedData(trainingDataUrl, validationDataUrl);
 
             var trainingData = trainingDataCsv
                 .Map(CsvParser.CsvToFeaturesWithLabel).Match(error =>
@@ -48,7 +62,7 @@ namespace KnnIris
                     .Pipe(Statistics.ExplainStatistics)
                     .Pipe(Console.WriteLine);
             }
-            
+
             Console.WriteLine("End");
         }
     }
